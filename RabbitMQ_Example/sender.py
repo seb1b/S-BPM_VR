2f12ec7710545d3efbc3a0ca66581523a11f17c8
Sebastian Bittel <sebastian.bittel@gmail.com>
2015-11-14 11:47:11 +0100

    rabbitMQ example added

---
+++ RabbitMQ_Example/sender.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env_python
+
+#CODE FROM https://www.rabbitmq.com/tutorials/tutorial-one-python.html
+
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+
+channel.queue_declare(queue='hello')
+
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print " [x] Sent 'Hello World!'"
+
+
+connection.close()
