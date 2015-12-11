3edf997f17d9958e16172fa9ea330e732e9e86da
Kai <haddung@gmx.de>
2015-12-11 09:48:37 -0800

    Beispielprozess V0.2  - AbstractVisualRepresentation hinzugefügt

--- Model/Beispielprozess.py
+++ Model/Beispielprozess.py
@@ -7,14 +7,19 @@ def buildHelper(manager, model):
 	# SID -> Subjects
 	sKonstrukteur = baseLayer.addSubject()
 	sKonstrukteur.label.append("Konstrukteur")
+	sKonstrukteur.hasAbstractVisualRepresentation.setPoint2D(0.1,0.3)
 	sPruefer = baseLayer.addSubject()
 	sPruefer.label.append("Prüfer")
+	sKonstrukteur.hasAbstractVisualRepresentation.setPoint2D(0.3,0.3)
 	sEntwicklungsleiter = baseLayer.addSubject()
 	sEntwicklungsleiter.label.append("Entwicklungsleiter")
+	sKonstrukteur.hasAbstractVisualRepresentation.setPoint2D(0.3,0.1)
 	sPrototypenbau = baseLayer.addSubject()
 	sPrototypenbau.label.append("Prototypenbau")
+	sKonstrukteur.hasAbstractVisualRepresentation.setPoint2D(0.5,0.3)
 	sSerienfertigung = baseLayer.addSubject()
 	sSerienfertigung.label.append("Serienfertigung")
+	sKonstrukteur.hasAbstractVisualRepresentation.setPoint2D(0.7,0.3)
 	# SID -> MessageExchanges
 	meKonstrukteurPruefer = baseLayer.addMessageExchange(sKonstrukteur, sPruefer)
 	meKonstrukteurPruefer.label.append("Änderungsantrag")
@@ -36,16 +41,21 @@ def buildHelper(manager, model):
 	# SBD -> States
 	state1 = behaviorPruefer.addReceiveState()
 	state1.label.append("Warten auf Antrag")
+	state1.hasAbstractVisualRepresentation.setPoint2D(0.1,0.1)
 	behaviorPruefer.setInitialState(state1)
 	state2 = behaviorPruefer.addFunctionState()
 	state2.label.append("Prüfe Antrag")
+	state1.hasAbstractVisualRepresentation.setPoint2D(0.3,0.1)
 	state3 = behaviorPruefer.addSendState()
 	state3.label.append("Bitte um Freigabe")
+	state1.hasAbstractVisualRepresentation.setPoint2D(0.5,0.1)
 	state4 = behaviorPruefer.addReceiveState()
 	state4.label.append("Ende")
+	state1.hasAbstractVisualRepresentation.setPoint2D(0.7,0.1)
 	state4.isFinalState = True
 	state5 = behaviorPruefer.addSendState()
 	state5.label.append("Informiere Konstrukteur")
+	state1.hasAbstractVisualRepresentation.setPoint2D(0.3,0.3)
 	
 	# SBD -> TransitionEdges
 	edge12 = behaviorPruefer.addReceiveTransition(state1, state2, meKonstrukteurPruefer)
@@ -66,3 +76,6 @@ buildHelper(manager, manager.model)
 
 #Save
 manager.saveAs("./tests/Beispielprozess.owl")
+#Load
+manager2 = PASS.ModelManager("./tests/Beispielprozess.owl")
+manager2.saveAs("./tests/Beispielprozess_copy.owl")
