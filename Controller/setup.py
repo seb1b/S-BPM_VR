493c5241b45fa928d4c086e4458f271aa5f44be2
uagnd <uagnd@student.kit.edu>
2015-12-14 22:05:53 +0100

    Fix python module hierarchy and package setup for Controller

--- Controller/setup.py
+++ Controller/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name = "Controller",
+setup(name = "controller",
 	version = "0.1",
 	description = "Implementation of the Controller",
 	url = "http://www.kit.edu",
 	author = "Daniel Spies, Zhilin Yuan",
 	author_email = "uagnd@student.kit.edu",
 	license = "Other",
-	packages = ["Controller"],
+	packages = ["controller"],
 	zip_safe = False)
