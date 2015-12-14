9094e0ccf5eab35d23ea3ddcb41d04d80314af7f
uagnd <uagnd@student.kit.edu>
2015-12-14 19:26:21 +0100

    Add python setup files for View and Controller

---
+++ Controller/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = "Controller",
+	version = "0.1",
+	description = "Implementation of the Controller",
+	url = "http://www.kit.edu",
+	author = "Daniel Spies, Zhilin Yuan",
+	author_email = "uagnd@student.kit.edu",
+	license = "Other",
+	packages = ["Controller"],
+	zip_safe = False)
---
+++ View/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = "View",
+	version = "0.1",
+	description = "Implementation of the View",
+	url = "http://www.kit.edu",
+	author = "Mirjam Jöchner",
+	author_email = "uagmw@student.kit.edu",
+	license = "Other",
+	packages = ["View"],
+	zip_safe = False)
