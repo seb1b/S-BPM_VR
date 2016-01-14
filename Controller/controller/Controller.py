8b8991b1e7c8bc23ef066af1871b27a8eee40a57
uagnd <uagnd@student.kit.edu>
2016-01-14 20:16:07 +0100

    Implement functions and change module setup

--- Controller/controller/Controller.py
+++ Controller/controller/Controller.py
@@ -2,7 +2,7 @@
 
 from PASS import ModelManager
 from view import View
-from hardware_main import VRHardware
+from hardware_main_neu import VRHardware
 
 class Controller:
 
@@ -13,14 +13,15 @@ class Controller:
 		self.current_model = None
 		self.current_view = None
 
-		self.press_position = None
+		self.press_position = {}
 		self.drag_position = None
-		self.release_position = None
+		self.release_position = {}
 
 		self.selected_objects = []
 		self.pressed_object = None
 		self.released_object = None
 		self.pressed_is_left = False
+		self.pressed_user_id = None
 
 		self.hw_main = VRHardware(self)
 
@@ -52,18 +53,22 @@ class Controller:
 
 		print("press({}, {}, {})".format(pos, user_id, is_left))
 
-		# TODO: check quick and dirty implementation
-
 		if self.current_view is not None:
 			assert self.current_model is not None
 			obj = self.current_view.get_object(pos[:2])
-			if obj is not None:
+			if obj is not None and self.pressed_user_id is None:
+				# CASE: press on existing obj(subject/message/menu bar item)
 				if obj not in self.selected_objects:
 					self.selected_objects.append(obj)
 				self.pressed_object = obj
 				self.pressed_is_left = is_left
+				self.pressed_user_id = user_id
+				self.drag_position = pos
 				self.current_view.set_highlight(obj, True)
-		self.press_position = pos
+			else:
+				# CASE: press on empty field or empty menu bar
+				pass
+		self.press_position[user_id] = pos
 
 		return None
 
@@ -94,7 +99,7 @@ class Controller:
 
 		if self.current_view is not None:
 			assert self.current_model is not None
-			if self.pressed_object is not None and self.pressed_is_left == is_left:
+			if self.pressed_object is not None and self.pressed_is_left == is_left and self.pressed_user_id == user_id:
 				# CASE: some object was released -> drag or select?
 				assert self.pressed_object in self.selected_objects
 				self.released_object = self.pressed_object
@@ -117,7 +122,7 @@ class Controller:
 					if new_obj is not None:
 						self.selected_objects.append(new_obj)
 					self.released_object = new_obj
-				elif sum(map(lambda x : x ** 2, [a - b for a, b in zip(self.press_position, pos)])) < 1.0:
+				elif sum(map(lambda x : x ** 2, [a - b for a, b in zip(self.press_position[user_id], pos)])) < 1.0:
 					# TODO: tweak squared distance threshold value
 					# CASE: some field object was selected
 					# nothing to do
@@ -135,8 +140,8 @@ class Controller:
 				self.selected_objects = []
 				# TODO: set highlight on empty field (for creating new object from menubar combo-command)
 			else:
-				print("RELEASE without previous PRESS - WTF???")
-		self.release_position = pos
+				print("case: x")
+		self.release_position[user_id] = pos
 
 		return None
 
@@ -209,6 +214,21 @@ class Controller:
 		:return: None
 		"""
 		print("fade_away()")
+		return None
+		
+	def fade_in(self, pos):
+		"""This function enters the inner sub-level of the object at the given 
+		position
+		
+		This function enters the inner sub-level of the object at the given 
+		position. 
+		
+		:param pos: the 3D position in normalized screen space coordinates
+		:type pos: float array of length 3
+		
+		:return: None
+		"""
+		print("fade_in({})".format(pos))
 		return None
 
 	def rotate(self, degrees):
@@ -291,15 +311,23 @@ class Controller:
 		self.rotate(90)
 		self.move_model([4.3, 0.4, 0.5], 33)
 		self.move_head([1.4, 0.0, -1.2], 180.2, 4)
+		
+	def test_bsp_prozess(self):
+		file_path = "/home/vrpraktikum/Projects/S-BPM_VR/Model/tests/Beispielprozess.owl"
+		self.models[file_path] = ModelManager(file_path)
+		self.views[file_path] = View()
+		self.current_model = self.models[file_path]
+		self.current_view = self.views[file_path]
+		self.current_model.addChangeListener(self.views[file_path].on_change)
+		self.views[file_path].on_change(self.current_model.model.hasModelComponent[0].subjects[0])
 
-	def init_debug_setup(self):
-		print("Initializing debug/testing model and view")
+	def init_empty(self):
 		file_path = "/tmp/temp_model.owl"
 		self.models[file_path] = ModelManager()
 		self.views[file_path] = View()
 		self.current_model = self.models[file_path]
 		self.current_view = self.views[file_path]
-		#self.current_model.addChangeListener(self.views[file_path].on_change)
+		self.current_model.addChangeListener(self.views[file_path].on_change)
 		# TODO: init model
 
 
